4afd41c4e58fc85de618913161e77fcc7da90783
Ibrahim Sowunmi <i.sowunmi97@gmail.com>
2025-06-27 20:52:44 +0100

    change log

--- playwright.config.js
+++ playwright.config.js
@@ -34,9 +34,9 @@ export default defineConfig({
     /* Action timeout */
     actionTimeout: 1000,
     /* Viewport size */
-    viewport: { width: 800, height: 600 },
+    viewport: { width: 1280, height: 720 },
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL: 'http://localhost:8080',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -81,9 +81,10 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'python app.py',
+    url: 'http://localhost:8080',
+    reuseExistingServer: !process.env.CI,
+    timeout: 120 * 1000,
+  },
 });
